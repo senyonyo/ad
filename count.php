75cc330522cad00477e76ea4ae18ae3fbe242836
Your Name <you@example.com>
2018-02-02 12:16:12 +0900

    add

--- count.php
+++ count.php
@@ -8,3 +8,20 @@
   $json=json_encode($obj);
   file_put_contents("save/log.jsons", $json+"\n", FILE_APPEND );
 ?>
+
+<html>
+  <head>
+    <title>お礼</title>
+  </head>
+  <body>
+    <p>ご協力ありがとうございました!</p>
+    <p>良い春休みを！</p>
+    <p>ご不明な点がございましたら、</br>
+       <a href="mailto:t16438as@sfc.keio.ac.jp">t16438as@sfc.keio.ac.jp</a></br>
+       <a href="mailto:senyo@sfc.wide.ad.jp">senyo@sfc.wide.ad.jp</a></br>
+       <a href="https://twitter.com/varia_unguenta">Twitter:@varia_unguenta</a></br>
+       まで、ご連絡ください。
+    </p>
+  </body>
+</html>
+
